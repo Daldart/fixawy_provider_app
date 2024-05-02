74a2265148fd80c92b3c89a8d4125dd96668b488
Ahmed Ghaith <61051522+Ahmed0Ghaith@users.noreply.github.com>
2024-05-03 02:25:31 +0300

    Withdrow Model Issue Fixed

--- lib/models/financial_Data.dart
+++ lib/models/financial_Data.dart
@@ -1,10 +1,11 @@
 class FinancialDataModel {
-  final int netTotal;
-  final int totalCommissions;
-  final int totalWithdrawn;
-  final int alreadyWithdrawn;
-  final int readyToWithdrawal;
-  final int pendingWithdrawal;
+  final double netTotal;
+  final double totalCommissions;
+  final double totalWithdrawn;
+  final double alreadyWithdrawn;
+  final double readyToWithdrawal;
+  final double pendingWithdrawal;
+  final String? currency;
 
   FinancialDataModel({
     required this.netTotal,
@@ -13,16 +14,18 @@ class FinancialDataModel {
     required this.alreadyWithdrawn,
     required this.readyToWithdrawal,
     required this.pendingWithdrawal,
+    this.currency,
   });
 
   factory FinancialDataModel.fromJson(Map<String, dynamic> json) {
     return FinancialDataModel(
-      netTotal: json['net_total'] ?? 0,
-      totalCommissions: json['total_commissions'] ?? 0,
-      totalWithdrawn: json['total_withdrawn'] ?? 0,
-      alreadyWithdrawn: json['already_withdrawn'] ?? 0,
-      readyToWithdrawal: json['ready_to_withdrawal'] ?? 0,
-      pendingWithdrawal: json['pending_withdrawal'] ?? 0,
+      netTotal: json['net_total']?.toDouble() ?? 0.0,
+      totalCommissions: json['total_commissions']?.toDouble() ?? 0.0,
+      totalWithdrawn: json['total_withdrawn']?.toDouble() ?? 0.0,
+      alreadyWithdrawn: json['already_withdrawn']?.toDouble() ?? 0.0,
+      readyToWithdrawal: json['ready_to_withdrawal']?.toDouble() ?? 0.0,
+      pendingWithdrawal: json['pending_withdrawal']?.toDouble() ?? 0.0,
+      currency: json['currency'] ?? '',
     );
   }
 }
